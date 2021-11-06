78b7c9298fd73988421888e98f9edbc74ef5bd1f
Sajidur78 <sajidur78@gmail.com>
2021-11-07 04:48:25 +0600

    Write bytes for aligning writer

--- src/Amicitia.IO/Binary/Extensions/BinaryValueWriterExtensions.cs
+++ src/Amicitia.IO/Binary/Extensions/BinaryValueWriterExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using System.IO;
+using System.Linq;
 
 namespace Amicitia.IO.Binary
 {
     public static class BinaryValueWriterExtensions
     {
-        public static void Align( this BinaryValueWriter writer, int alignment )
-            => writer.Seek( AlignmentHelper.Align( writer.Position, alignment ), SeekOrigin.Begin );
+        public static void Align(this BinaryValueWriter writer, int alignment)
+            => writer.WriteCollection( Enumerable.Repeat<byte>( 0, ( int )( AlignmentHelper.Align( writer.Position, alignment ) - writer.Position ) ) );
 
         public static void Skip( this BinaryValueWriter writer, int offset )
             => writer.Seek( offset, SeekOrigin.Current );
